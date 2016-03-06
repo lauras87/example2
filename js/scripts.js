877a1b4ffec51d3388999cb9b6537a64838f2a82
lauras87 <laurasiegel10@gmail.com>
2016-03-06 15:23:34 -0500

    2nd try contact form

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,11 @@ $("#contact-us").validate({
       method: "POST",
       data: {
         name: $(form).find("input[name='name']").val(),
-        mail: $(form).find("input[name='mail']").val(),
-        subject: $(form).find("input[name='subject']").val(),
+        _replyto: $(form).find("input[name='_replyto']").val(),
         message: $(form).find("textarea[name='message']").val()
       },
       dataType: "json"
-         success: function() {
+success: function() {
         $("#submit-success").fadeIn();
         $("#contact-us").fadeOut();
       },
