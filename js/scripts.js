2bbed0b258bc16bf45910feb69a75546236cdbb3
lauras87 <laurasiegel10@gmail.com>
2016-03-06 14:56:54 -0500

    1st try ajax contact form
    new scripts

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@ $(function(){
 	$('#containerm').mixItUp();
 
 });
+
+$("#contact-us").validate({
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/laurasiegel10@gmail.com", 
+      method: "POST",
+      data: {
+        name: $(form).find("input[name='name']").val(),
+        mail: $(form).find("input[name='mail']").val(),
+        subject: $(form).find("input[name='subject']").val(),
+        message: $(form).find("textarea[name='message']").val()
+      },
+      dataType: "json"
+         success: function() {
+        $("#submit-success").fadeIn();
+        $("#contact-us").fadeOut();
+      },
+      error: function() {
+        $("#submit-errors").fadeIn();        
+      }
+    });
+  }
+});
